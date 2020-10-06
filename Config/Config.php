9c6f282003069e49ed3afec81854cec2b29de3f6
RodriSuarez <rodri006@gmail.com>
2020-10-06 00:25:51 -0300

    mod config

--- Config/Config.php
+++ Config/Config.php
@@ -1,6 +1,6 @@
 <?php namespace Config;
 
-define("ROOT", dirname(__DIR__) . "/");
+define("ROOT", dirname(__DIR__) . "\\");
 //Path to your project's root folder
 define("FRONT_ROOT", "\\MoviePass\\");
 /*
@@ -15,6 +15,7 @@ define("API_KEY", "5c5be0bb9aae8be870e50088603452ef");
 define("LANGUAGE", "ES-es");
 
 ?>
+
 
 
 
