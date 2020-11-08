5848cf4dc7252e06dd356acbb7fdec591fd5595d
RodriSuarez <rodri006@gmail.com>
2020-11-08 02:37:15 -0300

    Se agrego la funcionalidad de enviar mail con el codigo qr

--- Controllers/showCinemaController.php
+++ Controllers/showCinemaController.php
@@ -216,7 +216,70 @@
     
            \QRcode::png($content, $filename, $level, $tam, $frameSize);
 
-           echo '<img src="' . $filename .'" alt="">';
+            $this->sendMail($show, $filename, $_SESSION['loggedUser']['email']);
+
+        }
+
+        public function sendMail(ShowCinema $show, $qr, $sendingTo){
+       
+            include_once('PHPMailer/PHPMailer/PHPMailer.php');
+            include_once('PHPMailer/PHPMailer/SMTP.php');
+            include_once('PHPMailer/PHPMailer/Exception.php');
+        
+            // Load Composer's autoloader
+
+            // Instantiation and passing `true` enables exceptions
+            $mail = new \PHPMailer\PHPMailer\PHPMailer(true);
+
+            try {
+                //Server settings
+                $mail->SMTPDebug = 0;# SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+                $mail->isSMTP();                                            // Send using SMTP
+                $mail->Host       = 'gmail-smtp-msa.l.google.com';                    // Set the SMTP server to send through
+                $mail->SMTPAuth   =   true;                                   // Enable SMTP authentication
+                $mail->Username   = 'noreply.moviepass@gmail.com';                     // SMTP username
+                $mail->Password   = 'comision1';                               // SMTP password
+                $mail->SMTPSecure = 'ssl';# PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+                $mail->SMTPOptions = array(
+                    'ssl' => array(
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true
+                    )
+                );
+                //Recipients
+                $mail->setFrom('noreply.moviepass@gmail.com', 'Movie Pass');
+                $mail->addAddress($sendingTo);     // Add a recipient
+            /*    $mail->addAddress('ellen@example.com');               // Name is optional
+                $mail->addReplyTo('info@example.com', 'Information');
+                $mail->addCC('cc@example.com');
+                $mail->addBCC('bcc@example.com');*/
+
+                // Attachments
+            $mail->addAttachment($qr);         // Add attachments
+            $mail->AddEmbeddedImage($qr, 'ticket');
+            //    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+                // Content
+                $mail->isHTML(true);                                  // Set email format to HTML
+                $mail->Subject = 'MoviePass '. $show->getMovie()->getTitle();
+                $mail->Body    = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+                                    <div>
+                                <h1 class="text-center">¡Llego tu entrada!</h1>
+                                <p>¡Felicidades! Llego tu entrada para ver <b>'. $show->getMovie()->getTitle() .'</b></p>
+                             <img src="cid:ticket" alt="img no disponible">
+                             <button type="btn btn-primary">Click Me!</button>
+                             </div>';
+                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        #     var_dump($mail);
+                $mail->send();
+                $message = 'Message has been sent';
+            } catch (\Exception $e) {
+                $message = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
+            
+
         }
 
         public function ShowAddView($movieID){
